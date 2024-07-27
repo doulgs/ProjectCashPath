faf0ae21e8cf746e28641626f0f81e63a9f77bfb
doulgs <douglasdsdomiciano@hotmail.com>
2024-07-27 12:41:07 -0300

    commit - including account routes and category routes

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
 
+// Middleware para verificação de autenticidade da empresa
+import { isCompanyAuth } from "./middlewares/isCompanyAuth";
+// Middleware para verificação de autenticidade do usuario
+import { isUserAuth } from "./middlewares/isUserAuth";
+
 import { CreateCompanyController } from "./controllers/companies/CreateCompanyController";
 import { AuthCompanyController } from "./controllers/companies/AuthCompanyController";
 import { DetailCompanyController } from "./controllers/companies/DetailCompanyController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
-
-// Middleware para verificação de autenticidade da empresa
-import { isCompanyAuth } from "./middlewares/isCompanyAuth";
-import { isUserAuth } from "./middlewares/isUserAuth";
+import { CreateCategoryController } from "./controllers/categories/CreateCategoryController";
+import { ListCategoryController } from "./controllers/categories/ListCategoryController";
+import { CreateAccountController } from "./controllers/accounts/CreateAccountController";
+import { ListAccountController } from "./controllers/accounts/ListAccountController";
 
 const router = Router();
 
@@ -22,5 +27,13 @@ router.get("/company/me", isCompanyAuth, new DetailCompanyController().handle);
 router.post("/user/create", isCompanyAuth, new CreateUserController().handle);
 router.post("/user/session", new AuthUserController().handle);
 router.get("/user/me", isUserAuth, new DetailUserController().handle);
+
+// Rotas para manipulação das categorias
+router.post("/user/category/create", isUserAuth, new CreateCategoryController().handle);
+router.get("/user/category/list", isUserAuth, new ListCategoryController().handle);
+
+// Rotas para manipulação das contas
+router.post("/user/account/create", isUserAuth, new CreateAccountController().handle);
+router.get("/user/account/list", isUserAuth, new ListAccountController().handle);
 
 export { router };
