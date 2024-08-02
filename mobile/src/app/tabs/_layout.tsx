150d159f3672f754e6d291b6169d8326c0340ec1
doulgs <douglasdsdomiciano@hotmail.com>
2024-08-02 15:16:20 -0300

    refactor: migrate from Tabs to Stack for navigation layout
    - Switched navigation layout from Tabs to Stack using expo-router.
    - Commented out and removed unnecessary tab bar configurations and styles.
    - Updated screen definitions to use Stack.Screen instead of Tabs.Screen.
    - Removed tab bar icons and tab-specific styles.

--- mobile/src/app/tabs/_layout.tsx
+++ mobile/src/app/tabs/_layout.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from "expo-router";
+import { Tabs, Stack } from "expo-router";
 import { View } from "react-native";
 
 import { colors } from "@/styles/colors";
@@ -13,23 +13,23 @@ import {
 export default function TabLayout() {
   return (
     <View className="flex-1 bg-zinc-800">
-      <Tabs
+      <Stack
         initialRouteName="summary"
         screenOptions={{
-          tabBarShowLabel: false,
-          tabBarStyle: {
+          //tabBarShowLabel: false,
+          /* tabBarStyle: {
             backgroundColor: colors.darkZinc[900],
             borderTopWidth: 0.5,
             borderTopColor: colors.darkZinc[500],
             height: 60,
-          },
-          tabBarActiveTintColor: colors.primary[200],
-          tabBarInactiveTintColor: colors.darkZinc[300],
+          }, */
+          //tabBarActiveTintColor: colors.primary[200],
+          //tabBarInactiveTintColor: colors.darkZinc[300],
           headerStyle: {
             backgroundColor: colors.darkZinc[900],
-            borderBottomWidth: 0,
+            //borderBottomWidth: 0,
             //borderBottomColor: colors.darkZinc[500],
-            elevation: 0,
+            //elevation: 0,
           },
           headerTitleStyle: {
             fontWeight: "light",
@@ -37,52 +37,52 @@ export default function TabLayout() {
           },
         }}
       >
-        <Tabs.Screen
+        <Stack.Screen
           name="index"
           options={{
             title: "Inicio",
             headerShown: false,
-            tabBarIcon: ({ color }) => <House size={24} color={color} />,
+            //tabBarIcon: ({ color }) => <House size={24} color={color} />,
           }}
         />
-        <Tabs.Screen
+        <Stack.Screen
           name="summary"
           options={{
             title: "",
-            tabBarIcon: ({ color }) => <ArrowRightLeft size={24} color={color} />,
+            //tabBarIcon: ({ color }) => <ArrowRightLeft size={24} color={color} />,
           }}
         />
-        <Tabs.Screen
+        <Stack.Screen
           name="[movimentsType]"
           options={{
             title: "Menu",
             headerShown: false,
-            tabBarIcon: ({ color }) => <DiamondPlus size={32} color={color} />,
-            tabBarStyle: { display: "none" },
+            //tabBarIcon: ({ color }) => <DiamondPlus size={32} color={color} />,
+            //tabBarStyle: { display: "none" },
           }}
-          listeners={() => ({
+          /* listeners={() => ({
             tabPress: (event) => {
               event.preventDefault();
               () => {};
             },
-          })}
+          })} */
         />
-        <Tabs.Screen
+        <Stack.Screen
           name="charts"
           options={{
             title: "Graficos",
-            tabBarIcon: ({ color }) => <ChartNoAxesColumn size={24} color={color} />,
+            //tabBarIcon: ({ color }) => <ChartNoAxesColumn size={24} color={color} />,
           }}
         />
-        <Tabs.Screen
+        <Stack.Screen
           name="profile"
           options={{
             title: "Perfil",
             headerShown: false,
-            tabBarIcon: ({ color }) => <CircleUserRound size={24} color={color} />,
+            // tabBarIcon: ({ color }) => <CircleUserRound size={24} color={color} />,
           }}
         />
-      </Tabs>
+      </Stack>
     </View>
   );
 }
