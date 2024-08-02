357dac4c81270915af4e717ae722f223f960ad0f
doulgs <douglasdsdomiciano@hotmail.com>
2024-08-02 15:25:54 -0300

    feat(menu): add MenuAction component with list of actions
    - Create MenuAction component
    - Define ActionProps type for menu actions
    - Add listAction array with predefined actions and icons
    - Implement renderAction function using useCallback for rendering actions in FlatList
    - Style the menu actions with TouchableOpacity and custom styles

---
+++ mobile/src/components/menuAction.tsx
@@ -0,0 +1,66 @@
+import { colors } from "@/styles/colors";
+import { BadgePlus, Boxes, CreditCard, LayoutGrid, Wallet } from "lucide-react-native";
+import { ReactNode, useCallback } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+
+type ActionProps = {
+  id: number;
+  title: string;
+  icon: () => ReactNode;
+  action: () => void;
+};
+
+const listAction: ActionProps[] = [
+  {
+    id: 1,
+    title: "Inserir Movimentação",
+    icon: () => <BadgePlus color={colors.zinc[200]} size={28} />,
+    action: () => {},
+  },
+  {
+    id: 2,
+    title: "listar Contas",
+    icon: () => <Wallet color={colors.zinc[200]} />,
+    action: () => {},
+  },
+  {
+    id: 3,
+    title: "listar Categorias",
+    icon: () => <Boxes color={colors.zinc[200]} />,
+    action: () => {},
+  },
+  {
+    id: 4,
+    title: "mais Opções",
+    icon: () => <LayoutGrid color={colors.zinc[200]} />,
+    action: () => {},
+  },
+];
+
+function MenuAction() {
+  const renderAction = useCallback(({ item }: { item: ActionProps }) => {
+    return (
+      <TouchableOpacity onPress={() => item.action()}>
+        <View className="h-28 w-32 p-3 mr-4 rounded-2xl bg-darkZinc-900 border border-zinc-800">
+          <View className="flex-1">{item.icon()}</View>
+          <Text className="text-darkZinc-100 text-base leading-none">
+            {item.title.replace(" ", "\n")}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }, []);
+
+  return (
+    <FlatList
+      horizontal
+      data={listAction}
+      keyExtractor={(item) => String(item.id)}
+      renderItem={renderAction}
+      ListFooterComponent={() => <View className="w-4" />}
+      className="px-4 mt-6"
+    />
+  );
+}
+
+export { MenuAction };
