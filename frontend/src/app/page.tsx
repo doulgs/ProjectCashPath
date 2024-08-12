9bc568a361c8118eec0283a2a3d7323a3a135206
doulgs <douglasdsdomiciano@hotmail.com>
2024-08-12 13:32:07 -0300

    chore(setup): install and configure Next.js with Tailwind CSS and Shadcn UI components
    - Initialized a new Next.js project.
    - Configured Tailwind CSS for styling.
    - Integrated Shadcn UI components into the project.
    - Set up the project structure to support responsive design and component-based development.
    
    Initial setup for the project is complete, with a responsive and customizable UI framework in place.

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import {} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <p>Welcome to CashPath!</p>
+    </main>
+  );
+}
